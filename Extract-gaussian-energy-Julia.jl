51df5c99a4e2ac544f3cbedafacf403233de80b1
lenhanpham <69336081+lenhanpham@users.noreply.github.com>
2023-01-22 23:42:11 +1030

    Optmized code

--- Extract-gaussian-energy-Julia.jl
+++ Extract-gaussian-energy-Julia.jl
@@ -1,12 +1,6 @@
 using Statistics
 using CSV
-#using NamedArrays
-
-### This snipper of Julia code was written by Le Nhan Pham (https://lenhanpham.github.io/) 
-### and is experimenting. Some of functions are not working well
-### This snippet of Julia code is used to extract energy from gaussian outputs with thermal correction, phase correction if solvent is used. 
-### Total Gibbs free energies will be sorted with the mostable structures on the top. 
-### Todo list: write output to a file. 
+using NamedArrays
 
 extractedData = Vector{Vector{Any}}()
 
@@ -14,85 +8,76 @@ extractedData = Vector{Vector{Any}}()
 function extract(file_name)
     fileNameVec = Vector{Any}()
 
-    scf = 000000
-    try
-        scf = parse(Float64, split(readlines(file_name)[findlast(x -> occursin("SCF Done", x), readlines(file_name))])[5])
-        #println(scf)
-    catch
+    # read entire file
+    file_lines = readlines(file_name)
 
+    # find last SCF Done line
+    scf = 0
+    try
+        scf = parse(Float64, split(file_lines[findlast(x -> occursin("SCF Done", x), file_lines)])[5])
+    catch
+        
     end
 
-    zpe = 000000
+    # find last Zero-point correction line
+    zpe = 0
     try
-        zpe = parse(Float64, split(readlines(file_name)[findlast(x -> occursin("Zero-point correction", x), readlines(file_name))])[3])
-        #println(zpe)
+        zpe = parse(Float64, split(file_lines[findlast(x -> occursin("Zero-point correction", x), file_lines)])[3])
     catch
-
+        
     end
 
-    tcg = 000000
+    # find last Thermal correction to Gibbs Free Energy line
+    tcg = 0 
     try
-        tcg = parse(Float64, split(readlines(file_name)[findlast(x -> occursin("Thermal correction to Gibbs Free Energy", x), readlines(file_name))])[7])
-        #println(tcg)
+        tcg = parse(Float64, split(file_lines[findlast(x -> occursin("Thermal correction to Gibbs Free Energy", x), file_lines)])[7])
     catch
-
+        
     end
 
-
-    etg = 000000
+    # find last Sum of electronic and thermal Free Energies line
+    etg = 0
     try
-        etg = parse(Float64, split(readlines(file_name)[findlast(x -> occursin("Sum of electronic and thermal Free Energies", x), readlines(file_name))])[8])
-        #println(etg)
+        etg = parse(Float64, split(file_lines[findlast(x -> occursin("Sum of electronic and thermal Free Energies", x), file_lines)])[8])
     catch
-
+        
     end
 
-
-    ezpe = 000000
+    # find last Sum of electronic and zero-point Energies line
+    ezpe = 0
     try
-        ezpe = parse(Float64, split(readlines(file_name)[findlast(x -> occursin("Sum of electronic and zero-point Energies", x), readlines(file_name))])[7])
-        #println(ezpe)
+        ezpe = parse(Float64, split(file_lines[findlast(x -> occursin("Sum of electronic and zero-point Energies", x), file_lines)])[7])
     catch
-
+        
     end
 
-
-    lf = 000000
+    # find first Frequencies line
+    lf = 0
     try
-        lf = parse(Float64, split(readlines(file_name)[findfirst(x -> occursin("Frequencies", x), readlines(file_name))])[3])
-        #println(lf)
+        lf = parse(Float64, split(file_lines[findfirst(x -> occursin("Frequencies", x), file_lines)])[3])
     catch
-
+        
     end
 
-    phaseCorr = 0
-    scrf = "no"
-    try
-        scrf = readlines(file_name)[findfirst(x -> occursin("scrf", x), readlines(file_name))]
-        #println(scrf)
-    catch
-        println("Gas phase")
-    end
-    if scrf == "no"
+    phaseCorr = "NO"
+    scrf = findfirst(x -> occursin("scrf", x), file_lines)
+    if isnothing(scrf)
         GibbsFreeHartree = etg
         phaseCorr = "NO"
-
     else
         GibbsFreeHartree = etg + 0.003031803235
         phaseCorr = "YES"
-
     end
 
     etgev = GibbsFreeHartree * 27.211396641308
     etgkj = GibbsFreeHartree * 2625.5002
 
-
     # check status of output
-    n = "unknow"
+    n = "unknown"
     try
-        n = split(readlines(file_name)[findlast(x -> occursin("Normal", x) || occursin("Error", x), readlines(file_name))])[1]
+        n = split(file_lines[findlast(x -> occursin("Normal", x) || occursin("Error", x), file_lines)])[1]
     catch
-
+        
     end
     if n == "Normal"
         status = "Done"
@@ -102,13 +87,11 @@ function extract(file_name)
         status = "Undone"
     end
 
-
-
-    append!(fileNameVec, [file_name, etgkj, lf, GibbsFreeHartree, etg, etgev, scf, zpe, status, phaseCorr])
+    fileNameVec = [file_name, etgkj, lf, GibbsFreeHartree, etg, etgev, scf, zpe, status, phaseCorr]
     return fileNameVec
 end
 
-function prinheader()
+function printheader()
     println("Output name                               ETG kJ/mol            Low FC          ETG a.u          ETG noCorr a.u         ETG eV                  SCFE             ZPE      Status   PCorr")
     println("-----------                               ----------            ------          -------               ------            ------                  ----             ----     ------   ----")
 end
@@ -125,9 +108,7 @@ function extractE()
     return extractedData
 end
 
-
-
-prinheader()
+printheader()
 extractE()
 
 for row in extractedData
@@ -135,4 +116,3 @@ for row in extractedData
     #print out deserved information
     println("$(lpad(string(row[1]), 35)) $(lpad(row[2], 25)) $(lpad(row[3], 10)) $(lpad(string(row[4]), 22)) $(lpad(string(row[5]), 15)) $(lpad(string(row[6]), 25)) $(lpad(string(row[7]), 18)) $(lpad(string(row[8]), 10)) $(lpad(row[9], 8)) $(lpad(row[10], 6))")
 end
-
