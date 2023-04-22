8f1e8890761d926b83c3d69787401c1cbbe0b0a8
lenhanpham <69336081+lenhanpham@users.noreply.github.com>
2023-04-22 23:05:18 +1000

    Add files via upload

--- fragmentation-of-xyz-for-Gaussian.py
+++ fragmentation-of-xyz-for-Gaussian.py
@@ -1,17 +1,18 @@
-# write a python function to read a text file and then split all lines to words
 import sys
-import os 
+import os
 
-#xyzfile = "Pre-KetylRad-Olefin-0.xyz"
-#newxyz = "Pre-KetylRad-Olefin-0-frag.xyz"
+# xyzfile = "Pre-KetylRad-Olefin-0.xyz"
+# newxyz = "Pre-KetylRad-Olefin-0-frag.xyz"
 
 xyzfile = sys.argv[1]
 newxyz = os.path.splitext(os.path.basename(xyzfile))[0] + "-frag.xyz"
 
+
 def read_file_split_lines(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         lines = [line.split() for line in file.readlines()[2:]]
     return lines
+
 
 def add_frag(lines, indices, addItems=",1"):
     for index in indices:
@@ -19,23 +20,29 @@ def add_frag(lines, indices, addItems=",1"):
     return lines
 
 
-
-
 lines = read_file_split_lines(xyzfile)
 totalAtoms = len(lines)
 
 ### set the number of atoms for each fragment: can be a range or a list of expected atom index
-frag1 = range(0, 26, 1)   
-frag2 = range(26,totalAtoms,1) 
+frag1 = range(0, 26, 1)
+frag2 = range(26, totalAtoms, 1)
 
 
-### add fragments 
-addFrag1 = add_frag(lines, frag1, ",1")  
+### add fragments
+addFrag1 = add_frag(lines, frag1, ",1")
 addFrag2 = add_frag(addFrag1, frag2, ",2")
 
-with open(newxyz, 'w') as xyzFrag:
-        xyzFrag.write(str(totalAtoms)+"\n")
-        xyzFrag.write(newxyz+"\n")
-        for line in addFrag2:
-            xyzFrag.write("{:<4s}{:>14.7f}{:>14.7f}{:>14.7f}{:>4s}\n".format(str(line[0]), float(line[1]), float(line[2]), float(line[3]), str(line[4])))
+with open(newxyz, "w") as xyzFrag:
+    xyzFrag.write(str(totalAtoms) + "\n")
+    xyzFrag.write(newxyz + "\n")
+    for line in addFrag2:
+        xyzFrag.write(
+            "{:<4s}{:>14.7f}{:>14.7f}{:>14.7f}{:>4s}\n".format(
+                str(line[0]),
+                float(line[1]),
+                float(line[2]),
+                float(line[3]),
+                str(line[4]),
+            )
+        )
 
