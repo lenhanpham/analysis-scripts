82b9c9b3fb7c15a1ab99ea92e2bc974aa1cddc9f
lenhanpham <69336081+lenhanpham@users.noreply.github.com>
2023-06-19 21:29:21 +0930

    Add files via upload

--- Xtract-gaussian.sh
+++ Xtract-gaussian.sh
@@ -1,21 +1,56 @@
 #! /bin/bash
-##############################################################################################################
-# This script is used to extract energies from Gaussian calculations				     	      #
-#      	     											              #
-# Output files should have extensions of .out or log                                                 	      #
-# Author: Le-Nhan Pham Deakin Uni	https://lenhanpham.github.io/                                         #
-##############################################################################################################
+##
+#### This script is written by Le Nhan Pham #####
+##### Deakin Uni, IFM #####
+
+
+### temperature needs to be specified as the first argument of this script
+### Usage: Thermo-component-analysis-with-improved-Electronic-higher-level-of-theory.shell TEMPERATURE_value 
+### if no temperature is set, 298.15 will be used.
+temp=$1
+
+if [ -z $temp ]; then
+temp=298.15 
+fi
+
+### Gibbs free energy of Phase change correction from gas phase Po = 1 atm = 101325 N/m2 to solution 
+### with concentration C = 1M = 1000 mol/m3, , R=8.314462618 J.K-1.mol-1
+### Delta_G_corr=RT*ln(P/Po) = RT*ln(nRT/VPo) = RT*ln((n/V)*(RT/Po)) = RT*ln(C*RT/Po)
+R=8.314462618
+C=1000
+### 1 atm = 101325 n/m2; 1 bar = 100000 n/m2
+Po=101325 
+
+kB=0.000003166811563
+
+GphaseCorr=$(echo "scale=8; (($R*$temp*l($C*$R*$temp/$Po)*0.0003808798033989866/1000))" | bc -l 2>/dev/null |  awk '{printf "%f", $0}' | cut -c 1-16) 
+
+
 
 # function for data extraction
 extract()
 {
-       	scf=$(grep "SCF Done" $file_name | tail -1 | awk '{print $5}')
+    scf=$(grep "SCF Done" $file_name | tail -1 | awk '{print $5}')
 	zpe=$(grep "Zero-point correction" $file_name |tail -1  | awk '{print $3}')
 	tcg=$(grep "Thermal correction to Gibbs Free Energy" $file_name |tail -1|awk '{print $7}')
 	etg=$(grep "Sum of electronic and thermal Free Energies" $file_name | tail -1|awk '{print $8}')
 	ezpe=$(grep "Sum of electronic and zero-point Energies" $file_name | tail -1|awk '{print $7}')
 #	lf=$(grep "Low frequencies" $file_name | tail -1 |awk '{print $4}'` # | sed s/-//)
 	lf=$(grep "Frequencies" $file_name | head -1 |awk '{print $3}')
+	nucleare=$(grep "nuclear repulsion energy" $file_name | tail -1 | awk '{print $4}')
+	
+	scrf=$(grep "scrf" $file_name | head -1 | awk '{print substr($0,0,5);}') 
+	if [ -z "$scrf" ]; then
+		GibbsFreeHartree=$etg
+		phaseCorr="NO"
+	else
+		### Phase correction term 7.96 kJ/mol = 0.003031803235 au
+		GibbsFreeHartree=$(echo "scale=8; ($etg+$GphaseCorr)" | bc 2>/dev/null |  awk '{printf "%f", $0}' | cut -c 1-16)
+		phaseCorr="YES"
+	fi 
+
+	etgev=$(echo "scale=8; $GibbsFreeHartree*27.211396641308" | bc 2>/dev/null |  awk '{printf "%f", $0}' | cut -c 1-16) 
+	etgkj=$(echo "scale=8; $GibbsFreeHartree*2625.5002" | bc 2>/dev/null |  awk '{printf "%f", $0}' | cut -c 1-16)
 
 	# check status of output
 	n=$(tail -10 $file_name|grep Normal)
@@ -24,29 +59,53 @@ extract()
 		if [ "k$n" != "k" ]; then
 	                askt="ERROR"
 		else
-			askt="UNFINISHED"
+			askt="UNDONE"
 		fi
 	else
 		askt="DONE"
 	fi
 
+	### total characters in file names
+	totalchar=$(printf "$file_name" | wc -m)
+	if [ $totalchar -gt 35 ]; then
+		startingCut=$(($totalchar-30))
+		file_name=$(printf "$file_name" | cut -c $startingCut-) 	
+	fi
+
 	# print out deserved information
-	printf "%40s %15s %20s %15s %15s %15s %15s\n" "$file_name"  "$ezpe" "$scf" "$zpe" "$etg" "$lf" "$askt"
+	printf "%35s %18s %10s %18s %18s %18s %18s %10s %8s %6s\n" "$file_name" "$etgkj" "$lf" "$GibbsFreeHartree" "$etg" "$nucleare" "$scf" "$zpe" "$askt" "$phaseCorr"
 
 	
 }
 
+prinheader(){
 # call function in all available output
-awk 'BEGIN { printf "%40s %15s %20s %15s %15s %15s %15s\n", "Output name", "E+ZPE", "SCFE",  "ZPE",  "ETG",  "Low FC",  "Status"
-             printf "%40s %15s %20s %15s %15s %15s %15s\n", "-----------", "-----","----", "----", "---",  "-------", "------" }'
+awk 'BEGIN { printf "%35s %18s %10s %18s %18s %18s %18s %10s %8s %6s\n", "Output name",  "ETG kJ/mol", "Low FC", "ETG a.u", "ETG noCorr a.u", "Nuclear E au", "SCFE", "ZPE ", "Status", "PCorr"
+             printf "%35s %18s %10s %18s %18s %18s %18s %10s %8s %6s\n", "-----------",  "----------", "------", "-------", "------", "------", "----", "----", "------",  "------" }' 
+}
 
 #for file_name  in `ls *.out`; do
 #	extract
 #done
 #echo " "
+extractE(){
 for file_name  in `ls *log`; do
         extract
 done
+}
+
+if [ -f "$(basename $(pwd))-$temp.results" ]; then 
+	rm $(basename $(pwd))-$temp.results 
+fi  
+
+
+printf "Temperature: $temp K. Make sure that temperature = $temp has been used in your input.\n" | tee $(basename $(pwd))-$temp.results 
+printf "Default concentration for phase correction: 1 M or 1000 mol/m3 \n" | tee -a $(basename $(pwd))-$temp.results 
+printf "Gibbs free correction for phase changing from 1 atm to 1 M: $GphaseCorr au \n" | tee -a $(basename $(pwd))-$temp.results 
+
+prinheader | tee -a $(basename $(pwd))-$temp.results
+
+extractE | sort -n -k2 2>&1 | tee -a $(basename $(pwd))-$temp.results 
 
 exit 0
 
